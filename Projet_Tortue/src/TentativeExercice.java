ddf7127843184abb9a889aeb420664cb0a40591e
doudi <doudi@DESKTOP-BHNJTHG>
2017-03-30 11:50:51 +0200

    azertyu

--- Projet_Tortue/src/TentativeExercice.java
+++ Projet_Tortue/src/TentativeExercice.java
@@ -16,46 +16,95 @@ public class TentativeExercice
 	private String evaluation;
 	private String commentaire;
 	
+	/**
+	 * Constructeur
+	 * 
+	 */
     public TentativeExercice{
     	
     }
     
+    /**
+	 * reinitialiser la tentative
+	 * 
+	 */
     private reset{
     	
     }
     
+    /**
+	 * obtenir l'evaluation pour l'exercice
+	 * 
+	 */
 	private getEvaluation(){
 	    	return(this.evaluation)
 	    }
 	
+	/**
+	 * Donner un évalutation pour la tentative
+	 * 
+	 */
 	private setEvaluation(String e){
     	this.evaluation = e
     }
     
+	/**
+	 * obtenir les commentaires de la tentative
+	 * 
+	 */
 	private getCommentaire(){
     	return(this.commentaire);
     }
 	
+	/**
+	 * saisir les commentaires de la tentative
+	 * 
+	 */
 	private setCommentaire(String c){
 		this.commentaire = c;
 	}
 	
+	/**
+	 * savoir si la tentative est a corriger
+	 * dans le cas où la tentative a été réalisé mais pas encore corrigé
+	 * 
+	 */
 	private getACorriger(){
 		return(this.aCorriger);
 	}
 	
+	
+	/**
+	 * permet de changer le booleen
+	 * TRUE : dans le cas ou la tentative est à corriger
+	 * FALSE: dans le cas ou l'exercice n'a pas été realisé ou a été corrigé
+	 */
 	private setACorriger(boolean ac){
 		this.aCorriger = ac;
 	}
 	
+	/**
+	 * permet de changer le booleen corrige
+	 * TRUE : dans le cas ou l'exercice vient d'être corrigé
+	 * FALSE: dans le cas où l'exercice n'a pas été corrige ou qu'il est à corriger
+	 */
 	private setCorrige(boolean c){
 		this.corriger = c;
 	}
 	
+	/**
+	 * permet de d'obtenir le boolean
+	 * TRUE : dans le cas ou l'exercice vient d'être corrigé
+	 * FALSE: dans le cas où l'exercice n'a pas été corrige ou qu'il est à corriger
+	 */
 	private getCorrige(boolean c){
 		return(this.corriger);
 	}
 	
+	/**
+	 * Permet d'enlever une action
+	 * Possible que 
+	 */
 	private undoAction(){
 		
 	}
