c6fc03380de23a8ad3db7c418d224542bf35b04b
doudi <doudi@172.17.69.217>
2017-04-03 18:02:23 +0200

    Blabla

--- Projet_Tortue/src/vue/CreationExercice.java
+++ Projet_Tortue/src/vue/CreationExercice.java
@@ -18,8 +18,10 @@ public class CreationExercice extends JFrame{
     private JMenuBar menu = new JMenuBar();
     private JMenu deconnexion = new JMenu("Deconnexion");
     private JMenu retour = new JMenu("Retour");
+    private ImageIcon image = new ImageIcon();
     
     //Creation des panels 
+    private JPanel panelGeneral1 = new JPanel();
     private JPanel panelGeneral = new JPanel(); // permet de regrouper l'ensemble des donnees
     private JPanel panelTortue = new JPanel();
     private JPanel panelTortue2 = new JPanel(); // panel qui va contenir les 3 boutons des tortues 
@@ -39,8 +41,9 @@ public class CreationExercice extends JFrame{
     
     //Creation des JTextField 
     private JTextField entreeNom = new JTextField();
-    entreeNom.setColumns(10);
     private JTextField entreeDate = new JTextField();
+    
+    private JLabel messImage = new JLabel("Sélectionnez votre image :");
     
     //Creation du bouton valider 
     private JButton valider = new JButton ("Valider");
@@ -53,12 +56,15 @@ public class CreationExercice extends JFrame{
 public CreationExercice(){
     this.setTitle("Creation de l'exercice");
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //this.setSize(800, 600);
     
     //ajout des éléments dans la barre des menus 
     menu.add(deconnexion);
     menu.add(retour);
     setJMenuBar(menu);
     
+    entreeNom.setColumns(10);
+    entreeDate.setColumns(10);
     panelNom.add(labelNom,BorderLayout.WEST);
     panelNom.add(entreeNom,BorderLayout.EAST);
     panelDate.add(labelDate,BorderLayout.WEST);
@@ -70,6 +76,11 @@ public CreationExercice(){
     panelInfo.add(panelNom);
     panelInfo.add(panelDate);
     
+    // CREE PANEL
+    panelImage = new JPanel(new BorderLayout(2,1));
+    panelImage.add(messImage);
+    panelImage.add(new JLabel(image));
+    
     //panelTortue2.setLayout(new GridLayout(1,3));
     panelTortue2.add(tClassique, BorderLayout.WEST);
     panelTortue2.add(tRapide, BorderLayout.CENTER);
@@ -79,20 +90,22 @@ public CreationExercice(){
     panelTortue.add(labelTortueDispo);
     panelTortue.add(panelTortue2);
     
-    //panelTortue.add(valider);
+    // panel general regrouppant tous les items
+    panelGeneral1.setLayout(new GridLayout(2,1));
+    panelGeneral1.add(panelInfo);
+   // panelGeneral1.add(panelImage);
+    panelGeneral1.add(panelTortue);
     
-    panelGeneral.setLayout(new GridLayout(3,1));
-    panelGeneral.add(panelInfo);
-    panelGeneral.add(panelImage);
-    panelGeneral.add(panelTortue);
+    //panelGeneral.setLayout(new BoxLayout(panelGeneral, BoxLayout.Y_AXIS));
+    panelGeneral.add(panelGeneral1);
+    panelGeneral.add(valider);
     
     this.add(panelGeneral);
     this.pack();
     this.setVisible(true);
-    
-    
-    
 }
+
+
 public static void main(String[] args){
         CreationExercice co= new CreationExercice();
     }
