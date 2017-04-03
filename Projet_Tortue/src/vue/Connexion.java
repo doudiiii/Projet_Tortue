164e1b4fbf821c45e35f62767716fe0d44b0da2e
doudi <doudi@172.17.69.217>
2017-04-03 11:12:28 +0200

    Blabla

---
+++ Projet_Tortue/src/vue/Connexion.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vue;
+import java.awt.BorderLayout;
+import javax.swing.*;
+/**
+ *
+ * @author Jennifer
+ */
+public class Connexion extends JPanel
+{
+    
+    JLabel labelIdentification;
+    PanelLabelButton panelEleve; 
+    PanelLabelButton panelProf; 
+    JPanel panelGeneral; 
+    
+    public Connexion ()
+    {
+        this.labelIdentification = new JLabel("Veuillez vous identifier :");
+        this.panelProf =  new PanelLabelButton (new ImageIcon("/Users/Jennifer/Desktop/professor.png"), new JLabel ("Professeur"));
+        this.panelEleve =  new PanelLabelButton (new ImageIcon("/Users/Jennifer/Desktop/graduate-boy.png"), new JLabel ("Eleve")); 
+        panelGeneral.add(panelProf,BorderLayout.WEST); 
+        panelGeneral.add(panelEleve,BorderLayout.EAST);
+        
+        
+    }
+
+    
+    
+}
