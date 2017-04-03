89d9473149ea023542d8fd9f3b046579c04769d9
Marine <Marine@DESKTOP-9Q9FVGA>
2017-04-03 09:16:14 +0200

    Exerciceok

--- Projet_Tortue/src/modele/Exercice.java
+++ Projet_Tortue/src/modele/Exercice.java
@@ -4,22 +4,31 @@ import javax.swing.*;
 import java.util.Date;
 
 /**
- * Created by moi on 30/03/2017.
+ * Created by Groupe3 on 30/03/2017.
  * permet la creation de l'objet Exercice et permet l'accès au variable via des getters nomExercice, date, ImageE
  */
 public class Exercice {
+	
     private String nomExercice;
     private Date date;
-    private ImageIcon ImageE;
+    private ImageIcon imageE;
+    private int numeroExo;
+    private ArrayList<Classe> lClasse= new ArrayList<Classe>();
 
-    public Exercice(String nomExercice, Date date, ImageIcon image) {
+    public Exercice(String nomExercice,int numE, ArrayList<Classe> listClasse, Date date, ImageIcon image) {
         this.nomExercice = nomExercice;
+        this.numeroExo = numE;
         this.date = date;
-        ImageE = image;
+        this.lClasse = listClasse;
+        imageE = image;
     }
 
     public String getNomExercice() {
         return nomExercice;
+    }
+    
+    public int getNumExo() {
+        return numeroExo;
     }
 
     public Date getDate() {
@@ -28,5 +37,9 @@ public class Exercice {
 
     public ImageIcon getImage() {
         return ImageE;
+    }
+    
+    public ArrayList<Classe> getListClasse() {
+        return lClasse;
     }
 }
