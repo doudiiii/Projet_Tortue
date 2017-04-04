98d7137b5be20b47872fe1943908890ba9f75585
doudi <doudi@172.17.69.217>
2017-04-04 16:35:33 +0200

    Blabla

---
+++ Projet_Tortue/src/vue/InterfaceMenuProf.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vue;
+import java.awt.*;
+import javax.swing.*;
+
+public class InterfaceMenuProf extends JFrame {
+    
+    JLabel labelIdentification;
+    PanelLabelBouton panelCrea; 
+    PanelLabelBouton panelEval;
+    PanelLabelBouton panelVisu; 
+    JPanel panelBouton;
+    JPanel panelGeneral; 
+    
+    public InterfaceMenuProf()
+    {
+        
+        panelBouton = new JPanel();
+        panelGeneral = new JPanel();
+        //test
+        this.panelCrea =  new PanelLabelBouton (new ImageIcon(".\\ressources\\Image\\creation.png"), new JLabel ("Creation"));
+        this.panelEval =  new PanelLabelBouton (new ImageIcon(".\\ressources\\Image\\Check.png"), new JLabel ("Evaluation")); 
+        this.panelVisu =  new PanelLabelBouton (new ImageIcon(".\\ressources\\Image\\Visu.png"), new JLabel ("Visualisation"));
+        
+        //panelBouton.setLayout(new GridLayout(3,1));
+        panelBouton.add(panelCrea); //,BorderLayout.NORTH
+        panelBouton.add(panelVisu);//,BorderLayout.CENTER
+        panelBouton.add(panelEval);//,BorderLayout.SOUTH
+        
+        //panelGeneral.add(panelBouton,BorderLayout.NORTH);
+        //panelGeneral.add(panelEval,BorderLayout.SOUTH);
+        
+        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Test");
+        this.setSize(500,200);
+        this.setVisible(true);
+        this.add(panelBouton);
+    }
+    
+    public static void main(String[] args){
+        InterfaceMenuProf co= new InterfaceMenuProf();
+    }
+    
+    
+    
+}
